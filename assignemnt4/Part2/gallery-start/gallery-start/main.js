7c78f5994785c9db03d4d868da96419d2eddda03
JaydenR52 <jayden.ricketts@dcmail.ca>
2025-04-04 08:46:54 -0400

    part
    commit 1

---
+++ assignemnt4/Part2/gallery-start/gallery-start/main.js
@@ -0,0 +1,15 @@
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
+
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+
+
+/* Looping through images */
+
+const newImage = document.createElement('img');
+newImage.setAttribute('src', xxx);
+newImage.setAttribute('alt', xxx);
+thumbBar.appendChild(newImage);
+
+/* Wiring up the Darken/Lighten button */
